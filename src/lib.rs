402c466fe5879056494eb3065c75835a26da6dd6
Wendell Smith <wackywendell@gmail.com>
2014-09-11 09:27:47 -0400

    Added isqrt function

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ use std::collections::TrieSet;
 
 use std::collections::HashMap;
 
+pub use primesieve::PrimeSet;
+
 pub mod primesieve;
 
 /// Count the number of occurrences of each value in an iterator
@@ -21,7 +23,7 @@ pub fn counter<K : std::hash::Hash + Eq, I : Iterator<K>>(mut list : I) -> HashM
 	counter
 }
 
-pub fn isqrt(n : uint) -> Option<uint> {
+pub fn isqrt_opt(n : uint) -> Option<uint> {
 	if n <= 1 {return Some(n);}
 	let mut x = n / 2;
 	let mut usedset = TrieSet::new();
@@ -41,10 +43,43 @@ pub fn isqrt(n : uint) -> Option<uint> {
 	Some(x)
 }
 
+pub fn isqrt(n : uint) -> uint {
+	if n <= 1 { return n; }
+	let mut x = n / 2;
+	let mut usedset = TrieSet::new();
+	
+	while x*x != n {
+		usedset.insert(x);
+		let lastx = x;
+		x = (x + n / x + 1) / 2;
+		//if usedset.contains(&x) {
+		if x == lastx {
+			if x*x < n {
+				return x;
+			} else {
+				return x-1;
+			}
+		}
+	}
+	return x
+}
+
+pub fn is_palindrome(n : uint) -> bool {
+	let s = n.to_string();
+	//let s_rev = String::from_utf8(s.into_bytes().iter().rev().collect());
+	let s_rev = s.as_slice().as_bytes().iter().rev();
+	let s_rev_vec : Vec<u8> = s_rev.map(|&b| b).collect();
+	let s_rev = String::from_utf8(s_rev_vec);
+	match s_rev {
+		Ok(s2) => {s == s2},
+		_ => false
+	}
+}
+
 #[test]
 fn test_square(){
-	assert_eq!(isqrt(4), Some(2));
-	assert_eq!(isqrt(5), None);
+	assert_eq!(isqrt_opt(4), Some(2));
+	assert_eq!(isqrt_opt(5), None);
 	
 	let mut ntests = vec![1,7,8,9,10,11,12,189654,4294967295];
 	
@@ -57,12 +92,51 @@ fn test_square(){
 	for &n in ntests.iter(){
 		println!("n: {}, n*n: {}", n, n*n);
 		assert!(n < std::num::pow(2u, std::uint::BITS / 2));
-		assert_eq!(isqrt(n*n), Some(n));
+		assert_eq!(isqrt_opt(n*n), Some(n));
 		if n > 1 {
-			assert_eq!(isqrt(n*n - 1), None);
+			assert_eq!(isqrt_opt(n*n - 1), None);
 		} else {
-			assert_eq!(isqrt(n*n - 1), Some(0));
+			assert_eq!(isqrt_opt(n*n - 1), Some(0));
 		}
-		assert_eq!(isqrt(n*n + 1), None);
+		assert_eq!(isqrt_opt(n*n + 1), None);
 	}
+}
+
+#[test]
+fn test_isqrt(){
+	assert_eq!(isqrt(0), 0);
+	let mut ntests = vec![1,7,8,9,10,11,12,189654,4294967295];
+	
+	for i in range(1u, 1001u){
+		let mut n = std::rand::random::<uint>();
+		n = n % std::num::pow(2u, std::uint::BITS / 2);
+		ntests.push(n);
+		ntests.push(i);
+	}
+	
+	for &n in ntests.iter() {
+		assert!(n < std::num::pow(2u, std::uint::BITS / 2));
+		let x = isqrt(n);
+		assert!(x*x <= n);
+		assert!((x+1)*(x+1) > n);
+		assert_eq!(isqrt(n*n - 1), n-1);
+		assert_eq!(isqrt(n*n + 1), n);
+	}
+}
+
+#[test]
+fn test_palindrome(){
+	assert!(is_palindrome(1));
+	assert!(is_palindrome(2));
+	assert!(is_palindrome(8));
+	assert!(!is_palindrome(10));
+	assert!(!is_palindrome(12));
+	assert!(!is_palindrome(100));
+	assert!(!is_palindrome(11110));
+	assert!(!is_palindrome(21111));
+	assert!(!is_palindrome(12131));
+	assert!(is_palindrome(11));
+	assert!(is_palindrome(111));
+	assert!(is_palindrome(232));
+	assert!(is_palindrome(181));
 }
